20e2cf544b2de3843203879acd0807c760773e01
Moody <Moody@DESKTOP-U7VA4BS>
2023-08-03 23:32:07 -0600

    se hicieron cambios segun la rubrica del nortech

--- BPO_CONTROL/FormTareas.vb
+++ BPO_CONTROL/FormTareas.vb
@@ -13,5 +13,45 @@
         formUsuario.Show()
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        ' Abrir el FormUsuario
+        Dim formUsuario As New FormUsuario(nombreUsuario)
+        formUsuario.Show()
+    End Sub
 End Class
